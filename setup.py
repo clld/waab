01422b90805ff4f7d4dc40f0badb9053b3f1c569
xrotwang <xrotwang@googlemail.com>
2018-01-13 21:22:12 +0100

    upgraded to clld 4.x

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup, find_packages
 
-requires = [
-    'clld>=3.2.0',
-    'clldmpg>=2.0.0',
-]
-
-tests_require = [
-    'WebTest',
-    'mock',
-]
 
 setup(
     name='waab',
@@ -31,8 +22,22 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=requires,
-    tests_require=tests_require,
+    install_requires=['clldmpg~=3.1'],
+    extras_require={
+        'dev': ['flake8', 'waitress'],
+        'test': [
+            'psycopg2',
+            'tox',
+            'mock',
+            'pytest>=3.1',
+            'pytest-clld',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+            'selenium',
+            'zope.component>=3.11.0',
+        ],
+    },
     test_suite="waab",
     entry_points="""\
     [paste.app_factory]
--- waab/tests/test_functional.py
+++ waab/tests/test_functional.py
@@ -1,37 +1,27 @@
-from clldutils.path import Path
-from clld.tests.util import TestWithApp
+import pytest
 
-import waab
+pytest_plugins = ['clld']
 
 
-class Tests(TestWithApp):
-    __cfg__ = Path(waab.__file__).parent.joinpath('..', 'development.ini').resolve()
-    __setup_db__ = False
-
-    def test_home(self):
-        self.app.get_html('/')
-
-    def test_pairs(self):
-        self.app.get_html('/pairs')
-        self.app.get_dt('/pairs?sSearch_0=Al&iSortingCols=1&iSortCol_0=0')
-        self.app.get_dt('/pairs?sSearch_1=Bo&iSortingCols=1&iSortCol_0=1')
-        self.app.get_dt('/pairs?iSortingCols=1&iSortCol_0=5')
-        self.app.get_html('/pairs/133')
-
-    def test_values(self):
-        self.app.get_dt('/values?iSortingCols=1&iSortCol_0=1')
-        self.app.get_dt('/values?parameter=1&sSearch_1=2')
-        self.app.get_dt('/values?pair=135&iSortingCols=1&iSortCol_0=1')
-
-    def test_parameters(self):
-        self.app.get_html('/parameters')
-        self.app.get_html('/parameters/1')
-        self.app.get_json('/parameters/1.geojson')
-
-    def test_languages(self):
-        self.app.get_html('/languages')
-        self.app.get_json('/languages.geojson')
-
-    def test_sources(self):
-        self.app.get_html('/sources')
-        self.app.get_dt('/sources?sEcho=1')
+@pytest.mark.parametrize(
+    "method,path",
+    [
+        ('get_html', '/'),
+        ('get_html', '/pairs'),
+        ('get_dt', '/pairs?sSearch_0=Al&iSortingCols=1&iSortCol_0=0'),
+        ('get_dt', '/pairs?sSearch_1=Bo&iSortingCols=1&iSortCol_0=1'),
+        ('get_dt', '/pairs?iSortingCols=1&iSortCol_0=5'),
+        ('get_html', '/pairs/133'),
+        ('get_dt', '/values?iSortingCols=1&iSortCol_0=1'),
+        ('get_dt', '/values?parameter=1&sSearch_1=2'),
+        ('get_dt', '/values?pair=135&iSortingCols=1&iSortCol_0=1'),
+        ('get_html', '/parameters'),
+        ('get_html', '/parameters/1'),
+        ('get_json', '/parameters/1.geojson'),
+        ('get_html', '/languages'),
+        ('get_json', '/languages.geojson'),
+        ('get_html', '/sources'),
+        ('get_dt', '/sources?sEcho=1'),
+    ])
+def test_pages(app, method, path):
+    getattr(app, method)(path)
